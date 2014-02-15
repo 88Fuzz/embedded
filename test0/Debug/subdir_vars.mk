8623563dce234ee04f1b93879edffbc163bd58f9
mreut <mreutman@gmail.com>
2014-02-15 15:47:07 -0500

    initial commit

---
+++ test0/Debug/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123ge6pm.cmd 
+
+LIB_SRCS += \
+/home/mreut/Development/CCS/tivaware/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../blinky.c \
+../tm4c123ge6pm_startup_ccs.c 
+
+OBJS += \
+./blinky.obj \
+./tm4c123ge6pm_startup_ccs.obj 
+
+C_DEPS += \
+./blinky.pp \
+./tm4c123ge6pm_startup_ccs.pp 
+
+C_DEPS__QUOTED += \
+"blinky.pp" \
+"tm4c123ge6pm_startup_ccs.pp" 
+
+OBJS__QUOTED += \
+"blinky.obj" \
+"tm4c123ge6pm_startup_ccs.obj" 
+
+C_SRCS__QUOTED += \
+"../blinky.c" \
+"../tm4c123ge6pm_startup_ccs.c" 
+
+
